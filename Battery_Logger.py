423eb2f6e1d8832f9a25362f08ab9ab26b433844
D3Zyre <onebitalpha@gmail.com>
2022-09-24 11:03:37 -0600

    created template functions

--- Battery_Logger.py
+++ Battery_Logger.py
@@ -1,3 +1,20 @@
 import psutil
+import time
+import datetime
 
 print(tuple(psutil.sensors_battery())) # (battery%, seconds_until_empty, is_connected_to_power)
+
+def get_battery_info():
+    """ Returns a tuple of the form:
+    (battery%, seconds_to_empty, is_connected_to_power)
+    
+    Note: seconds_to_empty will only be a number if the OS knows time to empty, otherwise it could be:
+    "POWERTIMEUNKNOWN" or "POWERTIMEUNLIMITED"
+    """
+    return tuple(psutil.sensors_battery())
+
+def append_to_csv():
+    """ Don't forget to create the csv if it doesn't exist already
+    """
+    pass
+
